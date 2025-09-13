2ab81a39bb2558bfc4aa044995b081c11f28537e
U-tale <hunter@ashfamily.net>
2025-09-13 15:02:01 -0400

    Add files via upload

---
+++ SilkSongU-tils/CrestHelper/CrestStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HutongGames.PlayMaker.Actions;
+using UnityEngine;
+using GO = UnityEngine.GameObject;
+
+namespace SilksongUTils.CrestHelper
+{
+    internal static class CrestStore
+    {
+
+        
+        public static List<HeroController.ConfigGroup> groups = new List<HeroController.ConfigGroup>();
+        
+        public static void StoreNewCrest(string Crest, HeroController.ConfigGroup config)
+        {
+            foreach (string Crests in CrestMaker.CrestList)
+            {
+                if (Crests == null)
+                {
+                    break;
+                }
+                else
+                {
+                    if (Crests == Crest)
+                    {
+                        groups.Add(config);  
+                    }
+                }
+            };
+        }
+
+
+        
+    }
+}
