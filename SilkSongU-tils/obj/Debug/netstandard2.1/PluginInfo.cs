591c74f8ff0fbf5d479592ea1453199f7d5e4497
U-tale <hunter@ashfamily.net>
2025-09-13 13:20:56 -0400

    First commit

---
+++ SilkSongU-tils/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SilksongUTils
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "silksongu_tils";
+        public const string PLUGIN_NAME = "SilkSongU-tils";
+        public const string PLUGIN_VERSION = "0.1.0";
+    }
+}
