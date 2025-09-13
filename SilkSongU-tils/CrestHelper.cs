aa8ae2b69fb16516983dcb6878c21e5271b1296e
U-tale <hunter@ashfamily.net>
2025-09-13 13:37:03 -0400

    Split the cresthelper from the main mod file

---
+++ SilkSongU-tils/CrestHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx;
+using SilkSongUtils;
+using UnityEngine;
+
+namespace SilksongUTils
+{
+    [BepInPlugin("SilkUtils", "SilksongUtils", "0.0.0.0")]
+    public abstract class CrestHelper : SilkSongU_tils
+    {
+        List<string> CrestList = ["Hunter", "Beast", "Witch", "Snail", "Witch", "Reaper"];
+
+        public void NewCrest(string CrestToAdd)
+        {
+            CrestList.Add(CrestToAdd);
+        }
+
+        public bool IsUnlocked { get; set; }
+        public void UnlockCrest(string CrestToUnlock)
+        {
+            foreach (string crest in CrestList)
+            {
+                if (crest != null)
+                {
+                    if (crest == CrestToUnlock)
+                    {
+                        // unlock the Crest
+                        return;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            throw new ArgumentException("That crest is either not handled, not added by an enabled dependency, or misspelled.");
+        }
+
+
+        public HeroControllerConfig? heroControllerConfig;
+
+        private HeroController.ConfigGroup? configGroup;
+        #region SlashGameObjects
+        public GameObject? activeRootObject { get; set; }
+        public GameObject? slashObject { get; set; }
+        public GameObject? slashUpObject { get; set; }
+        public GameObject? slashAltObject { get; set; }
+        public GameObject? slashDownObject { get; set; }
+        public GameObject? slashDashObject { get; set; }
+        public GameObject? slashWallObject { get; set; }
+        #endregion
+
+        public HeroController.ConfigGroup getConfigGroup() { return configGroup; }
+        public void NewCrestBaseAttacks()
+        {
+            configGroup = new HeroController.ConfigGroup
+            {
+                //alt slashes aren't accounted for.
+                Config = heroControllerConfig,
+                ActiveRoot = activeRootObject,
+                NormalSlashObject = slashObject,
+                AlternateSlashObject = slashAltObject,
+                UpSlashObject = slashUpObject,
+                DownSlashObject = slashDownObject,
+                DashStab = slashDashObject,
+                WallSlashObject = slashWallObject
+            };
+            if (configGroup == null) { return; }
+            configGroup.Setup();
+        }
+        public abstract void DashFSM();
+        public abstract void NailArtFSM();
+    }
+}
